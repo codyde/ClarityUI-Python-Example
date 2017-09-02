96a2d937de8d0782bd54a71e67b077d015ce2d91
codyde <codydearkland@gmail.com>
2017-09-02 15:14:57 -0700

    correcting bugs and updating readme

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Run this file by having Python3 installed on your system and running "python3 app.py" from a prompt in the current app's directory.  
-
 from flask import Flask, render_template, redirect, request, url_for, jsonify, session
 from flask_assets import Bundle, Environment
 import requests
@@ -8,8 +6,6 @@ from base64 import b64encode
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
-# Our bundling for css. This bundling starts in the static directory, so we just need to group them togehtre. You can see we create
-# Objects off of each of these that we reference in our header.html file
 env = Environment(app)
 js = Bundle('js/clarity-icons.min.js', 'js/clarity-icons-api.js',
             'js/clarity-icons-element.js', 'js/custom-elements.min.js')
@@ -17,12 +13,9 @@ env.register('js_all', js)
 css = Bundle('css/clarity-ui.min.css', 'css/clarity-icons.min.css')
 env.register('css_all', css)
 
-# App routes are how we build URLs in flask. / corresponds to the base, so http://localhost:5000/ would be the url. More examples later
 @app.route('/', methods=["GET", "POST"])
 def homepage():
-    if request.method == "POST":
-# This is where we grab the variables from the form wwithin tihin the index.html page. We do a simple if/then to say if the password is true
-# Then redirect to the next page 
+    if request.method == "POST": 
         attempted_username = request.form['username']
         print(attempted_username)
         attempted_password = request.form['password']
@@ -31,24 +24,21 @@ def homepage():
             session['logged_in'] = True
             session['wrong_pass'] = False
             session['username'] = request.form['username']
-            return redirect(url_for('vmlist'))
+            return redirect(url_for('homepage'))
         else:
             session['logged_in'] = False
             session['wrong_pass'] = True
     return render_template('index.html')
 
-# As mentioned above, this route corresponds to http://localhost:5000/vms
 @app.route('/vms')
 def vmlist():
-    req = requests.get('https://pyva.humblelab.com/rest/vcenter/vms') # Update this URL with the REST API details of your vCenter API. Mine is up so you can use it for testing. 
+    req = requests.get('https://pyva.humblelab.com/rest/vcenter/vms')
     req_json = req.json()
     return render_template('vms.html', vms = req_json)
 
 @app.route('/workflows')
 def workflows():
-# For vRO you'll want to update the URL to your vRO base url (https://hlcloud.humblelab.com for me) 
     url = 'https://hlcloud.humblelab.com'
-# Update the username:password field to match yours, i.e. administrator@vsphere.local:VMware1!
     value = b64encode(b"username:password").decode("ascii")
     headers = {
         'Authorization': 'Basic '+ value,
--- readme.md
+++ readme.md
@@ -5,8 +5,27 @@ This is a simple example of creating a Python Flask WebApp using Clarity UI
 There are a couple features in this page
 * Simple login functionality using the Clarity login control 
 * API connectivity to a rest API endpoint that lists out virtual machines 
-* API Connectivity to a vRO endpoint that lists out all vRO workflows with their Item HREF 
+* API Connectivity to a vRO endpoint that lists out all vRO workflows with their Item HREF
+* Jinja templating used extensively
+* Session details carried between pages 
 
 Angular functionality is missing from the ClarityUI so it's purely the CSS/JS components. 
+
+<h2>Flask Overview</h2>
+Flask is essentially a web platform for hosting page leveraging Python. It follows a traditional web application model for how it functions. Jinja templating is used to embed pages wtihin pages. You can see this example in how we include the header.html file in all other pages using the {% includes 'header.html' %} syntax. 
+
+<h2>Component Examples</h2>
+
+<h3>header.html</h3>
+Includes jinja templating references to carry this page int oothers 
+
+<h3>index.html</h3>
+Simple login form using Clarity UI. Uses the flask sessions module to tag sessions as "logged_in" = True to allow you to restrict certain pages if you wish
+
+<h3>vms.html</h3>
+We feed in a variable thats defined in our app.py and the iterate over it, using the table compononent from clarity to render the page. It's good and good for you. We do a simple for i in vms to iterate over it, and then drop a row component for each. 
+
+<h3>workflows.html</h3>
+We do a similar thing to what we did in vms.html but we iterate deeper into the array in this example 
 
 Enjoy! 
